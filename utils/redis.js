e96198eda392e595a58663721d92b192099cbd77
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-09 07:27:02 +0100

    feat: redis.js

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+const { createClient } = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => console.log(error));
+    this.connected = false;
+    this.client.on('connect', () => {
+      this.connected = this.isAlive();
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    await setAsync(key, duration, value);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
