6594dc753d0d4517443cb0c9ee02821c619d005b
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-10 01:05:51 +0100

    feat: add support for new routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 const router = Router();
 
@@ -9,5 +10,11 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
