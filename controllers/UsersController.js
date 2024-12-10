0814372ad9324618a4c0b2f240b9c81b9c2b54b9
Edwin Ade <edwinmbonyjr@gmail.com>
2024-12-10 01:05:28 +0100

    feat: UsersController update for me

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
@@ -9,6 +10,16 @@ class UsersController {
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
+  }
+
+  static async getMe(req, res) {
+    const token = req.get('X-Token');
+    const id = await redisClient.get(`auth_${token}`);
+    if (!id) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userDetails = await dbClient.getUserById(id);
+    return res.status(200).json(userDetails);
   }
 }
 
